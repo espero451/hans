736682f97331a922cba2bb2fa8bdc7edda954198
espero451 <253462134+espero451@users.noreply.github.com>
2026-02-15 00:38:11 +0100

    style: minor table style changes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
+// import Material from "@primeuix/themes/material";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import "./style.css";
@@ -12,6 +13,7 @@ import "./style.css";
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue, { theme: { preset: Aura } });
+// app.use(PrimeVue, { theme: { preset: Material } });
 app.mount("#app");
 
 // createApp(App).use(router).mount("#app");
